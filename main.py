95aeba7ae45e8bf474aa41b8ceebdd3db7b727dd
Silent-J <james23mcdermott@gmail.com>
2020-09-25 15:20:34 +0200

    create db function, resolved arg parse error

--- main.py
+++ main.py
@@ -19,6 +19,24 @@ def parse_args():
     
     return parser.parse_args()
 
+def create_connection(path):
+
+    connection = sqlite3.connect(os.path.join(frontpage_scraper.DB_PATH, 'frontpage-data.db'))    
+    c = connection.cursor()
+    
+    c.execute('''CREATE TABLE ARTICLES
+              (article_title text,
+              href text,
+              section text,
+              scrape_date date,
+              scrape_id integer,
+              author text,
+              pub_date date,
+              location text,
+              word_count text)''')
+              
+    print("created table ARTICLES in frontpage-data.db")
+    return connection
 
 if __name__=="__main__":
     
@@ -33,24 +51,21 @@ if __name__=="__main__":
     
     print("########## SCRAPING FRONTPAGE ##########")
     current_spider = ElPais_FrontPageSpider(URL)
-    
-    
+        
     # GET NEWLY CRAWLED DATA FROM THE CURRENT SPIDER
     new_data = current_spider.frontpage_df
     
     # FORMAT THE ARGS FOR THE BACKPAGE SPIDER
     backpage_scraper_args = list(zip(new_data['href'], new_data['scrape_id']))
     
-    print("\n########## SCRAPING BACKPAGES ##########")
-    
-    # PASS LIST OF URLS, IDS TO BACKPAGESPIDER 
+    print("\n########## SCRAPING BACKPAGES ##########")    
+
     # TODO: MULTIPROCESSING
     for t in tqdm(backpage_scraper_args):
         
-        ElPais_BackPageSpider(user_inp+t[0], t[1])  
-    
-    
-    # CONNECT TO DATABASE    
+        ElPais_BackPageSpider(URL+t[0], t[1])  
+        
+
     ARTICLE_METADATA = backpage_scraper.ARTICLE_METADATA
     
     DATA_TO_BE_INSERTED = pd.concat([new_data, pd.DataFrame(ARTICLE_METADATA)], axis=1)
@@ -58,26 +73,10 @@ if __name__=="__main__":
     DATA_TO_BE_INSERTED = DATA_TO_BE_INSERTED[['article_title', 'href', 'section', 'scrape_date',
                                                'scrape_id', 'author', 'pub_date', 'location', 'word_count']]
     
-    if not os.path.exists(os.path.join(frontpage_scraper.DB_PATH, 'frontpage-data.db')):
-        # MAKE CONNECTION
-        connection = sqlite3.connect(os.path.join(frontpage_scraper.DB_PATH, 'frontpage-data.db'))
-        # CURSOR
-        c = connection.cursor()
+    # CONNECT TO DB & INSERT DATA
+    if not os.path.exists(os.path.join(frontpage_scraper.DB_PATH, 'frontpage-data.db')):    
         
-        # CREATE TABLE
-        c.execute('''CREATE TABLE ARTICLES
-                  (article_title text,
-                  href text,
-                  section text,
-                  scrape_date date,
-                  scrape_id integer,
-                  author text,
-                  pub_date date,
-                  location text,
-                  word_count text)''')
-                  
-        print("created table ARTICLES in frontpage-data.db")
-    
+        create_connection(os.path.join(frontpage_scraper.DB_PATH, 'frontpage-data.db'))        
     else:
         connection = sqlite3.connect(os.path.join(frontpage_scraper.DB_PATH,'frontpage-data.db'))
         
